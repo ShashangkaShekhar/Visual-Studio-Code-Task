c17d57f7da977f2c3acd561f7d3da55ef7d88245
Shashangka Shekhar <shashangka@gmail.com>
2019-11-10 00:34:33 +0600

    Visual Studio Code Task

---
+++ src/home/component.ts
@@ -0,0 +1,25 @@
+﻿import { Component, OnInit, AfterViewInit} from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './app/home/component.html'
+})
+
+export class HomeComponent implements OnInit, AfterViewInit {
+    public title: any;
+    constructor(
+        private router: Router,
+        private titleService: Title) {
+
+    }
+
+    ngOnInit() {
+        this.titleService.setTitle("Home");
+        this.title = 'Angular8';
+    }
+
+    ngAfterViewInit() {}
+}
---
+++ src/module.ts
@@ -0,0 +1,65 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { Routes, RouterModule, RouteReuseStrategy } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import * as bootstrap from "bootstrap"
+import { HashLocationStrategy, LocationStrategy, PathLocationStrategy, APP_BASE_HREF } from '@angular/common';
+import { PreloadAllModules } from './apppreloader';
+
+//Components
+import { AppComponent } from './component';
+import { HomeComponent } from './home/component';
+import { ErrorComponent } from './error/component';
+
+const routes: Routes = [
+
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: '**',
+        component: ErrorComponent
+    },
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(
+            routes,
+            {
+                //enableTracing: false,
+                useHash: false,
+                preloadingStrategy: PreloadAllModules
+            })
+    ],
+
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        ErrorComponent],
+
+    bootstrap: [AppComponent],
+
+    providers: [
+        Title,
+        PreloadAllModules,
+        {
+            provide: LocationStrategy,
+            useClass: HashLocationStrategy
+        }
+    ],
+
+    exports: [RouterModule]
+})
+
+export class AppModule {
+
+}
